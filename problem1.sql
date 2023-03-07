263e72ef815e6d6c179058be069eb5d1776e9622
Acteonis <adosminkina@gmail.com>
2023-03-07 02:15:32 +0000

    the lab is completed

---
+++ problem1.sql
@@ -0,0 +1,16 @@
+/**
+         * Assignment: create a "post" table that has the following columns:
+         *       post table:
+         *       |   id  |     post        |        user_fk         |
+         *       ----------------------------------------------------
+         *       where the id is of type SERIAL PRIMARY KEY, post is of type varchar(255), and user_fk is of type
+         *       int.
+         *
+         * Note: The post column will hold the text content of the post and the user_fk column will be a foreign key to
+         * the site_user table's id column.
+         */
+         CREATE TABLE post (
+            id SERIAL PRIMARY KEY,
+            post varchar(255),
+            user_fk int REFERENCES site_user(id)
+            );
